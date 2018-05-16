2daeee67ac116b1677998bfb168d767bf9bb8eef
Adam Bohannon <abo46n2@gmail.com>
2018-05-16 16:56:38 -0700

    Installed Recharts, started implementing chart

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { BOX_SHADOW } from '../constants/style';
 
 const Wrapper = styled.div`
@@ -9,10 +16,46 @@ const Wrapper = styled.div`
   height: 400px;
   border-radius: 2px;
   box-shadow: ${BOX_SHADOW};
+  padding: 2rem 2rem 1rem 0;
+  box-sizing: border-box;
 `;
 
+const data = [
+  {
+    date: '12-01',
+    price: 50,
+  },
+  {
+    date: '12-02',
+    price: 100,
+  },
+  {
+    date: '12-03',
+    price: 1300,
+  },
+  {
+    date: '12-04',
+    price: 10,
+  },
+];
+
+const calculateMargin = num => ({
+  top: num, right: num, bottom: num, left: num,
+});
+
 const Graph = () => (
-  <Wrapper />
+  <Wrapper>
+    <ResponsiveContainer>
+      <LineChart
+        data={data}
+        margin={calculateMargin(8)}
+      >
+        <Line type="monotone" dataKey="price" stroke="#8884d8" />
+        <XAxis dataKey="date" />
+        <YAxis />
+      </LineChart>
+    </ResponsiveContainer>
+  </Wrapper>
 );
 
 export default Graph;
