d37efaa11bd73f8bb430ce8e09f28dc6afa38522
Adam Bohannon <abo46n2@gmail.com>
2018-06-07 13:29:59 -0700

    Added module.parent check

--- index.js
+++ index.js
@@ -22,4 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
+if (!module.parent) {
+  app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
+}
+
