749eff002073bda6a7eceab4352396a690f7c625
Adam Bohannon <abo46n2@gmail.com>
2018-06-07 12:11:15 -0700

    Fixed express.static dir

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
