e3bebdc02e823ee5eaf3c5421af1296736d26387
Adam Bohannon <abo46n2@gmail.com>
2018-07-22 21:40:27 -0700

    Updated static path

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
