196392c6226de5c20835d2e0c69ab42a15fef570
Adam Bohannon <abo46n2@gmail.com>
2018-07-22 20:51:35 -0700

    updated app.use in server

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
-  AA_API_KEY: process.env.AA_API_KEY,
 };
 
--- index.js
+++ index.js
@@ -13,15 +13,16 @@ mongoose.connect(keys.MONGODB_URI).then(
 );
 
 const app = express();
+
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+const path = require('path');
 
-  const path = require('path');
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 const PORT = process.env.PORT || 5000;
