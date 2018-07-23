52bfad6784ae83a7e358f7bbbded0032e07444b8
Adam Bohannon <abo46n2@gmail.com>
2018-07-22 21:29:01 -0700

    Added path.join and __dirname to express.static

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(bodyParser.json());
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
