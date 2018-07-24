12fdd42298a688e50c2785af2fe32eb83cac2282
Adam Bohannon <abo46n2@gmail.com>
2018-07-23 18:15:45 -0700

    meh

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+  console.log('path', path.join(__dirname, 'client/build'));
 }
 
 const PORT = process.env.PORT || 5000;
